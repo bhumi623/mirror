230835124b325ecacf69943c5f7d22c5f05f232e
bhumi623 <bhumiagarwal130@gmail.com>
2026-04-05 12:37:32 +0530

    Day4- Registration API endpoint with DRF serializer

---
+++ backend/users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only = True,
+        min_length = 8
+    )
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'name', 'preferred_language']
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password'],
+            name = validated_data.get('name', ''),
+            preferred_language = validated_data.get('preferred_language', 'en')
+        )
+        return user
